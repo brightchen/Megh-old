d0b1d5dfc90a53dc7b88b9b1ecd44af83b8c8912
bright <bright@bright-mac.local>
2015-10-26 17:18:23 -0700

    snapshot works

--- demos/dimensions/src/main/java/com/datatorrent/demos/dimensions/telecom/operator/AppDataSnapshotServerAggregate.java
+++ demos/dimensions/src/main/java/com/datatorrent/demos/dimensions/telecom/operator/AppDataSnapshotServerAggregate.java
@@ -1,14 +1,25 @@
 package com.datatorrent.demos.dimensions.telecom.operator;
 
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.common.collect.Maps;
+
 import com.datatorrent.api.Context.OperatorContext;
 import com.datatorrent.lib.appdata.gpo.GPOMutable;
 import com.datatorrent.lib.appdata.schemas.DimensionalConfigurationSchema;
+import com.datatorrent.lib.appdata.schemas.FieldsDescriptor;
+import com.datatorrent.lib.appdata.schemas.Type;
 import com.datatorrent.lib.appdata.snapshot.AbstractAppDataSnapshotServer;
 import com.datatorrent.lib.dimensions.DimensionsEvent.Aggregate;
 import com.datatorrent.lib.dimensions.aggregator.AggregatorRegistry;
 
 public class AppDataSnapshotServerAggregate extends AbstractAppDataSnapshotServer<Aggregate>
 {
+  private static final transient Logger logger = LoggerFactory.getLogger(AppDataSnapshotServerAggregate.class);
+      
   private String eventSchema;
   private transient DimensionalConfigurationSchema dimensitionSchema;
   
@@ -23,9 +34,32 @@ public class AppDataSnapshotServerAggregate extends AbstractAppDataSnapshotServe
   @Override
   public GPOMutable convert(Aggregate aggregate)
   {
-    aggregate.getAggregates().setFieldDescriptor(dimensitionSchema.getDimensionsDescriptorIDToAggregatorIDToOutputAggregatorDescriptor().get(aggregate.getDimensionDescriptorID()).get(aggregate.getAggregatorID()));
+    final FieldsDescriptor aggregatesFd = dimensitionSchema.getDimensionsDescriptorIDToAggregatorIDToOutputAggregatorDescriptor().get(aggregate.getDimensionDescriptorID()).get(aggregate.getAggregatorID());
+    aggregate.getAggregates().setFieldDescriptor(aggregatesFd);
     
-    return aggregate.getAggregates();
+    final FieldsDescriptor keysFd = dimensitionSchema.getDimensionsDescriptorIDToKeyDescriptor().get(aggregate.getDimensionDescriptorID());
+    GPOMutable keys = aggregate.getKeys();
+    keys.setFieldDescriptor(keysFd);
+    
+    {
+      Type fieldType = keysFd.getType("deviceModel");
+      logger.info("The type of field '{}' is '{}'", "deviceModel", fieldType);
+    }
+    
+    String deviceModel = keys.getFieldString("deviceModel");
+    
+    Map<String, Type> fieldToType = Maps.newHashMap();
+    fieldToType.put("deviceModel", Type.STRING);
+    fieldToType.put("downloadBytes", Type.LONG);
+    
+    FieldsDescriptor fd = new FieldsDescriptor(fieldToType);
+    
+    GPOMutable gpo = new GPOMutable(fd);
+    gpo.setField("deviceModel", deviceModel);
+    gpo.setField("downloadBytes", aggregate.getAggregates().getFieldLong("downloadBytes"));
+    
+    return gpo;
+    
   }
 
   public String getEventSchema()

f292b87324cff041b3863ac8c5c1d4d9949f647e
Timothy Farkas <tim@datatorrent.com>
2015-11-23 18:00:50 -0800

    Added filtering for CS operator

--- demos/dimensions/src/main/java/com/datatorrent/demos/dimensions/telecom/operator/CustomerServiceEnrichOperator.java
+++ demos/dimensions/src/main/java/com/datatorrent/demos/dimensions/telecom/operator/CustomerServiceEnrichOperator.java
@@ -5,11 +5,12 @@ import com.datatorrent.api.DefaultOutputPort;
 import com.datatorrent.api.annotation.InputPortFieldAnnotation;
 import com.datatorrent.common.util.BaseOperator;
 import com.datatorrent.demos.dimensions.telecom.model.CustomerService;
+import com.datatorrent.demos.dimensions.telecom.model.EnrichedCDR;
 import com.datatorrent.demos.dimensions.telecom.model.EnrichedCustomerService;
 
 public class CustomerServiceEnrichOperator extends BaseOperator {
 
-  
+
   @InputPortFieldAnnotation(optional = true)
   public final transient DefaultInputPort<CustomerService> inputPort = new DefaultInputPort<CustomerService>()
   {
@@ -20,11 +21,20 @@ public class CustomerServiceEnrichOperator extends BaseOperator {
     }
   };
   public final transient DefaultOutputPort<EnrichedCustomerService> outputPort = new DefaultOutputPort<EnrichedCustomerService>();
-  
+
 
   public void processTuple(CustomerService tuple)
   {
     EnrichedCustomerService enriched = EnrichedCustomerService.fromCustomerService(tuple);
-    outputPort.emit(enriched);
+
+    if (filter(enriched)) {
+      outputPort.emit(enriched);
+    }
+  }
+
+  private boolean filter(EnrichedCustomerService enriched)
+  {
+    String zipCode = enriched.getZipCode();
+    return zipCode.startsWith("93") || zipCode.startsWith("94") || zipCode.startsWith("95") || zipCode.startsWith("96");
   }
 }
